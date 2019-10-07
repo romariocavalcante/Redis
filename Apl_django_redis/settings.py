8916317c6322bdf86fbdc063fc1141642deaa5de
Romário Cavalcante <romariocavalcant@gmail.com>
2019-10-07 11:48:12 -0300

    atualização do projeto

--- Apl_django_redis/settings.py
+++ Apl_django_redis/settings.py
@@ -119,9 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/cliente/templates/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "img"),
-    'cliente/templates/',
+    os.path.join(BASE_DIR, "static"),
 ]
