23ec0953c8286e0d3f6b0b8e7029876b7a56e12c
Romário Cavalcante <romariocavalcant@gmail.com>
2019-10-08 05:14:50 -0300

    Atualização da madrugada

--- cliente/models.py
+++ cliente/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class herois(models.Model):
+
+    nome = models.CharField('Nome', max_length=150)
+
+    universo = models.CharField('Nome', max_length=150)
+
+    habilidade = models.CharField('Nome', max_length=150)
+
+    def __str__(self):
+        return self.nome
+
--- cliente/views.py
+++ cliente/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
+from .models import herois
 
+#@cache_page(60) #tempo de vida do cache
 def home(request):
-	return render(request,'index.html')
+	#import pdb;pdb.set_trace()
+	lista = herois.objects.all()
+	return render(request,'conteudo.html', {'lista':lista})
+'''
+	form = PostForm()
+	if(request.method == 'POST'):
+		form = PostForm(request.POST)
+		if(form.is_valid()):
+			post_nome = form.cleaned_data['nome']
+			post_universo = form.cleaned_data['universo']
+			post_habilidade = form.cleaned_data['habilidade']
+			new_post = Post(nome=post_nome, universo=post_universo, habilidade=post_habilidade )
+			new_post.save()
+			return redirect('')
+	elif(request.method == 'GET'):	
+
+'''
+	
+
+
+    
+        
